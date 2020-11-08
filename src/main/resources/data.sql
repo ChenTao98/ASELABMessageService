0d041ecc8abfd7a9333b40ab382f49527562dc0a
Chen <384490967@qq.com>
2020-11-08 10:49:52 +0800

    modify

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO message (message_id,type,content,status,user_id,tag,tip,sent_time)
-VALUES
-(1, "type","content","status","chentao","tag","2020-11-06 11:11:11"),
-(2, "type","content","status","chentao","tag","2020-11-06 11:12:11"),
-(3, "type","content","status","1630201","tag","2020-11-06 11:13:11");
