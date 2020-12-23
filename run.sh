bc5f3fd18b6cac0cc579647643ed61a0d1a55da5
Chen <384490967@qq.com>
2020-12-23 21:50:35 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean package -DskipTests
 docker build -t aselab:message .
-docker run -p 21000:21000 -d --name aselabmessageservice aselab:message
+docker run -p 8085:8085 -d --name aselabmessageservice aselab:message
